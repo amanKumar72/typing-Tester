353b15fc467fbcd65f1c2bd4320b2f4e759f7d4c
amanKumar72 <amankumar721088@gmail.com>
2024-02-04 06:12:06 -0800

    home-page

---
+++ jsFiles/home.js
@@ -0,0 +1,12 @@
+function next(){
+    let givenTime=document.querySelector(".time");
+    let givenType=document.querySelector(".type");
+    localStorage.setItem("time",givenTime.value);
+    localStorage.setItem("type",givenType.value);
+    window.location.href="../htmlFiles/index.html";
+
+}
+
+document.querySelector("#options").addEventListener("change",()=>{
+    window.location.href=this.value;
+})
